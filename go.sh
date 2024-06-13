3cdcda3f6b8eb9160a9547f026a5ededbee17def
Kiran Garimella <kiran@broadinstitute.org>
2024-06-13 12:06:51 -0400

    Modified test script to pull down three bam files and format fasta files

--- go.sh
+++ go.sh
@@ -4,10 +4,26 @@ set -euxo pipefail
 
 cargo build --release >/dev/null 2>&1
 
-./target/debug/hidive fetch \
+./target/release/hidive fetch \
 	-r \
-	-o HG002.HLA-A.bam \
 	-l chr6:29,940,532-29,947,870 \
+	-o HG002.HLA-A.bam \
 	gs://fc-bb12940d-f7ba-4515-8a98-42de84f63c34/HPRC_grch38/PBCCSWholeGenome/HG002/alignments/HG002.bam
 
-samtools index HG002.HLA-A.bam 
+./target/release/hidive fetch \
+	-r \
+	-l chr6:29,940,532-29,947,870 \
+	-o HG00438.HLA-A.bam \
+	gs://fc-bb12940d-f7ba-4515-8a98-42de84f63c34/HPRC_grch38/PBCCSWholeGenome/HG00438/alignments/HG00438.bam
+
+./target/release/hidive fetch \
+	-r \
+	-l chr6:29,940,532-29,947,870 \
+	-o HG02559.HLA-A.bam \
+	gs://fc-bb12940d-f7ba-4515-8a98-42de84f63c34/HPRC_grch38/PBCCSWholeGenome/HG02559/alignments/HG02559.bam
+
+samtools fasta HG002.HLA-A.bam | sed '/^>/s/$/\|HG002/' > HG002.HLA-A.fasta
+samtools fasta HG00438.HLA-A.bam | sed '/^>/s/$/\|HG00438/' > HG00438.HLA-A.fasta
+samtools fasta HG02559.HLA-A.bam | sed '/^>/s/$/\|HG02559/' > HG02559.HLA-A.fasta
+
+cat HG002.HLA-A.fasta HG00438.HLA-A.fasta HG02559.HLA-A.fasta > HLA-A.fasta
