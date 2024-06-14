468d585b2b90a8845413b9121503029520611cf5
Kiran Garimella <kiran@broadinstitute.org>
2024-06-14 14:22:44 -0400

    Added GFA reading

--- src/hidive/src/build.rs
+++ src/hidive/src/build.rs
@@ -4,6 +4,9 @@ use std::path::PathBuf;
 
 use bio::io::fasta::{IndexedReader, Reader, Record};
 
+use flate2::read::GzDecoder;
+use serde_json::Value;
+
 // Import the skydive module, which contains the necessary functions for building graphs
 use skydive;
 
@@ -12,7 +15,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
-use std::io::Write;
+use std::io::{self, BufRead, BufReader, Write};
 use std::sync::Mutex;
 
 pub fn get_reference_kmer_profile(ref_hla: &str, k: usize) -> Vec<String> {
@@ -536,6 +539,67 @@ pub fn filter_undersupported_edges(
     filtered_edges // Return the owned HashMap
 }
 
+pub struct GraphicalGenome {
+    pub anchor: HashMap<String, Value>,
+    pub edges: HashMap<String, Value>,
+    pub outgoing: HashMap<String, Vec<String>>,
+    pub incoming: HashMap<String, Vec<String>>,
+}
+
+impl GraphicalGenome {
+    pub fn load_graph(filename: &str) -> io::Result<GraphicalGenome> {
+        let file = File::open(filename)?;
+        let reader: Box<dyn BufRead> = if filename.ends_with(".gz") {
+            Box::new(BufReader::new(GzDecoder::new(file)))
+        } else {
+            Box::new(BufReader::new(file))
+        };
+        let mut anchor_dict = HashMap::new();
+        let mut edge_dict = HashMap::new();
+        let mut outgoing: HashMap<String, Vec<String>> = HashMap::new();
+        let mut incoming: HashMap<String, Vec<String>> = HashMap::new();
+        for line in reader.lines() {
+            let line = line?.trim_end().to_string();
+            if line.starts_with('S') {
+                let itemlist: Vec<&str> = line.split('\t').collect();
+                let name = itemlist[1];
+                let seq = itemlist[2];
+                let annotation = &itemlist[3][5..];
+
+                let json_value: Value = serde_json::from_str(annotation).unwrap();
+
+                let mut value = json_value;
+
+                value["seq"] = Value::String(seq.to_string());
+
+                if name.starts_with('A') {
+                    anchor_dict.insert(name.to_string(), value);
+                } else if name.starts_with('E') {
+                    edge_dict.insert(name.to_string(), value);
+                }
+            } else if line.starts_with('L') {
+                let itemlist: Vec<&str> = line.split('\t').collect();
+                let src = itemlist[1];
+                let dst = itemlist[3];
+                outgoing
+                    .entry(src.to_string())
+                    .or_default()
+                    .push(dst.to_string());
+                incoming
+                    .entry(dst.to_string())
+                    .or_default()
+                    .push(src.to_string());
+            }
+        }
+        Ok(GraphicalGenome {
+            anchor: anchor_dict,
+            edges: edge_dict,
+            outgoing: outgoing,
+            incoming: incoming,
+        })
+    }
+}
+
 pub fn start(
     output: &PathBuf,
     loci_list: &Vec<String>,
@@ -605,5 +669,9 @@ pub fn start(
         //     .collect();
 
         // println!("{:?}", seqlist);
+
+        // let graph_filename = 
+        let graph = GraphicalGenome::load_graph(output.to_str().unwrap()).unwrap();
+        println!("{:?}", graph.anchor)
     }
 }
