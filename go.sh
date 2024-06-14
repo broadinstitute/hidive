96f22688672f6dd38ad5bfb87f43849538713a29
Kiran Garimella <kiran@broadinstitute.org>
2024-06-14 00:08:48 -0400

    Added updated build command to the go.sh script

--- go.sh
+++ go.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-cargo build --release >/dev/null 2>&1
+cargo build --release
 
 ./target/release/hidive fetch \
 	-r \
@@ -22,10 +22,10 @@ cargo build --release >/dev/null 2>&1
 	-o HG02559.HLA-A.bam \
 	gs://fc-bb12940d-f7ba-4515-8a98-42de84f63c34/HPRC_grch38/PBCCSWholeGenome/HG02559/alignments/HG02559.bam
 
-samtools fasta HG002.HLA-A.bam | sed '/^>/s/$/\|HG002/' > HG002.HLA-A.fasta
+samtools fasta HG002.HLA-A.bam   | sed '/^>/s/$/\|HG002/' > HG002.HLA-A.fasta
 samtools fasta HG00438.HLA-A.bam | sed '/^>/s/$/\|HG00438/' > HG00438.HLA-A.fasta
 samtools fasta HG02559.HLA-A.bam | sed '/^>/s/$/\|HG02559/' > HG02559.HLA-A.fasta
 
 cat HG002.HLA-A.fasta HG00438.HLA-A.fasta HG02559.HLA-A.fasta > HLA-A.fasta
 
-./target/release/hidive build HLA-A.fasta GCA_000001405.15_GRCh38_no_alt_analysis_set.fa
+./target/release/hidive build -l chr6:29,940,532-29,947,870 -o HLA-A.gfa HLA-A.fasta GCA_000001405.15_GRCh38_no_alt_analysis_set.fa
