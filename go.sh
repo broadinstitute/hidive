21bc5b01f08f1ac8307b84b25f671c5f8d94ffc4
hangsuunc <suhang@broadinstitute.org>
2024-10-22 13:38:31 -0400

    added docker files

--- go.sh
+++ go.sh
@@ -24,10 +24,10 @@ mkdir -p $OUTPUT
 
 ./target/release/hidive fetch \
 	-l $LOCUS \
-	-o $OUTPUT/HG00438.maternal.GRCh38_no_alt.$NAME.fasta \
-	gs://fc-4310e737-a388-4a10-8c9e-babe06aaf0cf/working/HPRC/HG00438/assemblies/year1_f1_assembly_v2_genbank/alignment/assembly-to-reference/HG00438.maternal.GRCh38_no_alt.bam
+	-o $OUTPUT/HG002.maternal.GRCh38_no_alt.$NAME.fasta \
+	gs://fc-4310e737-a388-4a10-8c9e-babe06aaf0cf/working/HPRC_PLUS/HG002/assemblies/year1_f1_assembly_v2_genbank/alignment/assembly-to-reference/HG002.maternal.CHM13Y_EBV.bam
 
-minimap2 -ayYL --MD --eqx -x map-hifi -R '@RG\tID:maternal\tSM:maternal' $OUTPUT/HG00438.maternal.GRCh38_no_alt.$NAME.fasta $OUTPUT/HG00438.maternal.GRCh38_no_alt.$NAME.fasta | \
+minimap2 -ayYL --MD --eqx -x map-hifi -R '@RG\tID:maternal\tSM:maternal' /Users/suhang/Analysis/hidive/MHC-HG002.1.fa $OUTPUT/HG00438.maternal.GRCh38_no_alt.$NAME.fasta | \
 	samtools sort -O BAM --write-index -o $OUTPUT/HG00438.maternal.GRCh38_no_alt.$NAME.bam
 
 # paternal haplotype
@@ -42,21 +42,22 @@ minimap2 -ayYL --MD --eqx -x map-hifi -R '@RG\tID:maternal\tSM:maternal' $OUTPUT
 
 ./target/release/hidive fetch \
 	-l $LOCUS \
-	-o $OUTPUT/HG00438.LR.$NAME.fasta \
-	gs://fc-bb12940d-f7ba-4515-8a98-42de84f63c34/HPRC_grch38/PBCCSWholeGenome/HG00438/alignments/HG00438.bam
+	-o $OUTPUT/HG002.LR.$NAME.fasta \
+	gs://fc-bb12940d-f7ba-4515-8a98-42de84f63c34/HPRC_grch38/HG002/HG002.bam
 
-minimap2 -ayYL --MD --eqx -x map-hifi -R '@RG\tID:lr\tSM:lr' $OUTPUT/HG00438.maternal.GRCh38_no_alt.$NAME.fasta $OUTPUT/HG00438.LR.$NAME.fasta | \
-	samtools sort -O BAM --write-index -o $OUTPUT/HG00438.LR.$NAME.bam
+minimap2 -ayYL --MD --eqx -x map-hifi -R '@RG\tID:lr\tSM:lr' /Users/suhang/Analysis/hidive/MHC-HG002.1.fa $OUTPUT/HG002.LR.$NAME.fasta | \
+	samtools sort -O BAM --write-index -o $OUTPUT/HG002.LR.$NAME.bam
 
 # rescue
 
 ./target/release/hidive rescue \
-	-o $OUTPUT/HG00438.SR.$NAME.fasta \
-	-f $OUTPUT/HG00438.LR.$NAME.fasta \
-	gs://fc-1ee08173-e353-4494-ad28-7a3d7bd99734/working/HPRC/HG00438/raw_data/Illumina/child/HG00438.final.cram
+	-r /Users/suhang/Analysis/hidive_backup/GCA_000001405.15_GRCh38_no_alt_analysis_set.fa \
+	-o $OUTPUT/HG002.SR.mrna.$NAME.fasta \
+	-f $OUTPUT/HG002.LR.$NAME.fasta \
+	/Users/suhang/Analysis/hg002_gm24385.mrna.grch38.cram
 
-minimap2 -ayYL --MD --eqx -x sr -R '@RG\tID:sr\tSM:sr' $OUTPUT/HG00438.maternal.GRCh38_no_alt.$NAME.fasta $OUTPUT/HG00438.SR.$NAME.fasta | \
-	samtools sort -O BAM --write-index -o $OUTPUT/HG00438.SR.$NAME.bam
+minimap2 -ayYL --MD --eqx -x sr -R '@RG\tID:sr\tSM:sr' /Users/suhang/Analysis/hidive/MHC-HG002.1.fa $OUTPUT/HG002.SR.mrna.$NAME.fasta | \
+	samtools sort -O BAM --write-index -o $OUTPUT/HG002.SR.mrna.$NAME.bam
 
 # filter
 
@@ -65,7 +66,7 @@ minimap2 -ayYL --MD --eqx -x sr -R '@RG\tID:sr\tSM:sr' $OUTPUT/HG00438.maternal.
 	-l $OUTPUT/HG00438.LR.$NAME.fasta \
 	$OUTPUT/HG00438.SR.$NAME.fasta
 
-minimap2 -ayYL --MD --eqx -x sr -R '@RG\tID:sr-filt\tSM:sr-filt' $OUTPUT/HG00438.maternal.GRCh38_no_alt.$NAME.fasta $OUTPUT/HG00438.SR.$NAME.filtered.fasta | \
+minimap2 -ayYL --MD --eqx -x sr -R '@RG\tID:sr-filt\tSM:sr-filt' $OUTPUT/HG002.maternal.GRCh38_no_alt.$NAME.fasta $OUTPUT/HG002.SR.$NAME.filtered.fasta | \
 	samtools sort -O BAM --write-index -o $OUTPUT/HG00438.SR.$NAME.filtered.bam
 
 # co-assemble
