b29df5f712561c5f8688d7c83188170a45f5844e
kvg <kvg@users.noreply.github.com>
2024-12-02 23:43:50 +0000

    Deploying to gh-pages from @ broadinstitute/hidive@fa10056ff7c61836e13e690f5b4adbb6566cd93c 🚀

---
+++ trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["hidive",[["impl ValueEnum for <a class=\"enum\" href=\"hidive/rescue/enum.SearchOption.html\" title=\"enum hidive::rescue::SearchOption\">SearchOption</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[160]}
