d045957f1d1e120724e12ec3a2428d9896d4c35f
kvg <kvg@users.noreply.github.com>
2024-12-10 21:14:20 +0000

    Deploying to gh-pages from @ broadinstitute/hidive@bf1aca8befdbc98d345006afae05301fff9e066a 🚀

--- hidive/call/sidebar-items.js
+++ hidive/call/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["add_variant_records","build_haplotypes","initialize_vcf_header","phase_variants","prepare_matrix","start","write_haplotypes"]};
+window.SIDEBAR_ITEMS = {"fn":["add_variant_records","build_haplotypes","initialize_vcf_header","phase_variants","prepare_matrix","prepare_matrix_new","start","write_haplotypes"]};
