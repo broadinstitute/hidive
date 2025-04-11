c3bbfdb7f7e49c6ce75c6f79acc31956964d2f8a
kvg <kvg@users.noreply.github.com>
2025-04-11 12:36:51 +0000

    Deploying to gh-pages from @ broadinstitute/hidive@3394932f419ef2f63b3b130ca9d0b085bb3e4a6d 🚀

--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["skydive",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"skydive/link/struct.Link.html\" title=\"struct skydive::link::Link\">Link</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"skydive/record/struct.Record.html\" title=\"struct skydive::record::Record\">Record</a>"]]]]);
+    var implementors = Object.fromEntries([["skydive",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"skydive/link/struct.Link.html\" title=\"struct skydive::link::Link\">Link</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"skydive/record/struct.Record.html\" title=\"struct skydive::record::Record\">Record</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/fmt/trait.UpperHex.js
+++ trait.impl/core/fmt/trait.UpperHex.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["skydive",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"skydive/edges/struct.Edges.html\" title=\"struct skydive::edges::Edges\">Edges</a>"]]]]);
+    var implementors = Object.fromEntries([["skydive",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"skydive/edges/struct.Edges.html\" title=\"struct skydive::edges::Edges\">Edges</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
